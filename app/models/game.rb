b616df40e694cb699a284e3a2d0a6b0fb898b31e
Michael Stone <codemjstone06@gmail.com>
2019-04-15 06:23:24 +0000

    working on setting up pieces to show on the board

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,50 +1,55 @@
 class Game < ApplicationRecord
 
+  before_create :initialize_board!
+
   belongs_to :white_player, class_name: 'Player'
   belongs_to :black_player, class_name: 'Player'
   has_many :pieces
+  has_many :players
 
-  def setup_pieces
+  def initialize_board!
     (1..8).each do |num|
-      pieces.create(type: 'Pawn', position_x: num, position_y: 2)
+      pieces.create(type: 'Pawn', game_id: self.id, player_id: white_player, position_x: num, position_y: 2)
     end 
 
     (1..8).each do |num|
-      pieces.create(type: 'Pawn', player: black_player, position_x: num, position_y: 7)
+      pieces.create(type: 'Pawn', game_id: self.id, player: black_player, position_x: num, position_y: 7)
     end 
 
     [1, 8].each do |num|
-      pieces.create(type: 'Rook', player: white_player, position_x: num, position_y: 1)
+      pieces.create(type: 'Rook', game_id: self.id, player: white_player, position_x: num, position_y: 1)
     end 
 
     [1, 8].each do |num|
-      pieces.create(type: 'Rook', player: black_player, position_x: num, position_y: 8)
+      pieces.create(type: 'Rook', game_id: self.id, player: black_player, position_x: num, position_y: 8)
     end 
 
     [2, 7].each do |num|
-      pieces.create(type: 'Knight', player: white_player, position_x: num, position_y: 1)
+      pieces.create(type: 'Knight', game_id: self.id, player: white_player, position_x: num, position_y: 1)
     end 
 
     [2, 7].each do |num|
-      pieces.create(type: 'Knight', player: black_player, position_x: num, position_y: 8)
+      pieces.create(type: 'Knight', game_id: self.id, player: black_player, position_x: num, position_y: 8)
     end 
 
     [3, 6].each do |num|
-      pieces.create(type: 'Bishop', player: white_player, position_x: num, position_y: 1)
+      pieces.create(type: 'Bishop', game_id: self.id, player: white_player, position_x: num, position_y: 1)
     end 
 
     [3,6].each do |num|
-      pieces.create(type: 'Bishop', player: black_player, position_x: num, position_y: 8)
+      pieces.create(type: 'Bishop', game_id: self.id, player: black_player, position_x: num, position_y: 8)
     end 
 
-    pieces.create(type: 'Queen', player: white_player, position_x: 4, position_y: 1)
+    pieces.create(type: 'Queen', game_id: self.id, player: white_player, position_x: 4, position_y: 1)
 
-    pieces.create(type: 'Queen', player: black_player, position_x: 5, position_y: 8)
+    pieces.create(type: 'Queen', game_id: self.id, player: black_player, position_x: 5, position_y: 8)
 
-    pieces.create(type: 'King', player: white_player, position_x: 5, position_y: 1)
+    pieces.create(type: 'King', game_id: self.id, player: white_player, position_x: 5, position_y: 1)
   
-    pieces.create(type: 'King', player: black_player, position_x: 4, position_y: 8)
-  end 
+    pieces.create(type: 'King', game_id: self.id, player: black_player, position_x: 4, position_y: 8)
+  end
+
+
 
 
 end
