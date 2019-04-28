a943f710eb12a279a3e0bc68bd1724fe3955ddc5
Isabella DiMambro <belladimambro@gmail.com>
2019-04-28 19:43:46 +0000

    capture logic

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -37,6 +37,25 @@ class Piece < ApplicationRecord
 
   end  
 
+  def move_to!(new_x, new_y)
+    #check to see if there is a piece ('blocking piece') in location already
+    blocklocking_piece_array = Piece.where(position_x: new_x, position_y: new_y)
+    array_length = blocking_piece_array.length
+    if array_length > 0
+      blocking_piece = blocking_piece_array.take 
+      #if blocking piece is same color, return error message
+      if blocking_piece.piece_color == @piece_color
+        raise 'invalid move: cannot replace your own piece'
+      end
+      #if blocking piece is opposite color, set block piece's coordinates to nil
+      blocking_piece.update_attributes(position_x: nil, position_y: nil)  
+    end
+    #update active piece's position to new coordinates
+    update_attributes(position_x: new_x, position_y: new_y)
+      
+  end
+
+
 end
 
 
