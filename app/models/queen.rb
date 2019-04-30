b34d1a0f5128790bf1bccdfc6a18a099655b465d
jasyw5 <john.stahl24@gmail.com>
2019-04-30 00:31:52 +0000

    updated queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,18 @@
 class Queen < Piece
+  def initialize(x, y)
+      @x = x
+      @y = y
+  end
+
+  def valid_move?(x, y)
+    if x == (0..7) && x != x && y == @y
+      return true
+    elsif x == @x && y == (0..7) && y != y
+      return true
+    elsif if ((@y - @x) == (y - x) || (@y + @x) == (y + x))
+      return true
+    else
+      return false
+    end
+  end
 end
